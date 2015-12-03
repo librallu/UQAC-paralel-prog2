31dbc220f4ddc343939fcd4667ae627d59081349
Luc Libralesso <libralesso.l@gmail.com>
2015-12-03 15:21:35 -0500

    copy

---
+++ copy.sh
@@ -0,0 +1,8 @@
+scp * 192.168.237.65:~/Devoir3/
+scp * 192.168.237.66:~/Devoir3/
+scp * 192.168.237.67:~/Devoir3/
+scp * 192.168.237.68:~/Devoir3/
+scp * 192.168.237.69:~/Devoir3/
+scp * 192.168.237.70:~/Devoir3/
+scp * 192.168.237.71:~/Devoir3/
+echo "COPY DONE!"
